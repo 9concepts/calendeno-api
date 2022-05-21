fec5ffa5dd8ba1f618c801d293c895fb0a2a2c68
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-05-22 02:35:23 +0900

    Return user information after authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,27 @@
 class SessionsController < ApplicationController
   def create
     user_info = request.env["omniauth.auth"]
-    render json: user_info.to_h
+
+    user = User.find_or_initialize_by(email: user_info["email"])
+    # NOTE: Create or update the user.
+    user.attributes = user_params
+    user.save
+
+    # TODO: 暫定的にユーザー情報を返している。
+    render json: UserResource.new(user).serialize
+  end
+
+  private
+
+  def user_params
+    id_token = request.env["omniauth.auth"]["extra"]["id_info"]
+
+    {
+      email: id_token["email"],
+      name: id_token["name"],
+      image: id_token["picture"],
+      iss: id_token["iss"],
+      sub: id_token["sub"]
+    }
   end
 end
